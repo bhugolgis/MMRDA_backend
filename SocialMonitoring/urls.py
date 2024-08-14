67bf5c0fa785f582665a1a2afb1df7d42c209521
pranavkolambkar23 <pranavkolambkar23@gmail.com>
2024-08-14 11:59:46 +0530

    change endpoints and create delete api for social app

--- SocialMonitoring/urls.py
+++ SocialMonitoring/urls.py
@@ -9,23 +9,26 @@ urlpatterns = [
     path('labourCampdetailsViewSearch' , labourCampdetailsViewSearch.as_view() , name = "labourCampdetailsView"),
  
     path ('pap' , PapView.as_view() , name = "project affected Person "),
-    path ('pap/<int:id>' , PapUpdateView.as_view() , name = "get-pap "),
-    path ('pap/<int:id>' , PapUpdateView.as_view() , name = "update-pap"),
-    path ('pap/<int:id>/rud' , PapRetrieveDestroyView.as_view() , name = "retrieve-destroy-pap"),
+    path ('pap/<int:id>' , PapGetUpdateDeleteView.as_view() , name = "get-pap "),
+    path ('pap/<int:id>' , PapGetUpdateDeleteView.as_view() , name = "update-pap"),
+    path ('pap/<int:id>' , PapGetUpdateDeleteView.as_view() , name = "delete-pap"),
     path ('paplist' , PapListView.as_view() , name = "project affected Person List "),
 
     # Rehab spelling is inconsistent (rehabilitation)
     path('rehabitationpapid/<str:PAPID>' , RehabilatedPAPIDView.as_view() , name = "rehabitationPapID"),
     path('rehabilitation' , RehabilitationView.as_view() , name = "rehabitation"),
-    path('rehabilitation/<int:id>', RehabilitationView.as_view(), name='get-rehabilitation'),
-    path('rehabilitation/<int:id>', RehabilitationUpdateView.as_view(), name='update-rehabilitation'),
+    path('rehabilitation/<int:id>', RehabilitationGetUpdateDeleteView.as_view(), name='get-rehabilitation'),
+    path('rehabilitation/<int:id>', RehabilitationGetUpdateDeleteView.as_view(), name='update-rehabilitation'),
+    path('rehabilitation/<int:id>', RehabilitationGetUpdateDeleteView.as_view(), name='delete-rehabilitation'),
         
     path ('constructionsite' , constructionSiteView.as_view() , name = "constructionSiteView"),
-    path ('construction-site/<int:id>', constructionSiteView.as_view() , name = "get-construction-site"),
-    path ('construction-site/<int:id>', ConstructionSiteUpdateView.as_view() , name = "update-construction-site"),
+    path ('construction-site/<int:id>', ConstructionSiteGetUpdateDeleteView.as_view() , name = "get-construction-site"),
+    path ('construction-site/<int:id>', ConstructionSiteGetUpdateDeleteView.as_view() , name = "update-construction-site"),
+    path ('construction-site/<int:id>', ConstructionSiteGetUpdateDeleteView.as_view() , name = "delete-construction-site"),
     path ('constructionsiteList' , ConstructionSiteListView.as_view() , name = "ConstructionSiteListView"),
 
     path ('labourcamp' , LabourCampView.as_view() , name = "LabourCampDetailsView"),
-    path('labour-camp/<int:id>', LabourCampUpdateView.as_view() , name =  "get-labour-camp"),
-    path('labour-camp/<int:id>', LabourCampUpdateView.as_view() , name =  "update-labour-camp"),
+    path('labour-camp/<int:id>', LabourCampUpdateGetDeleteView.as_view() , name =  "get-labour-camp"),
+    path('labour-camp/<int:id>', LabourCampUpdateGetDeleteView.as_view() , name =  "update-labour-camp"),
+    path('labour-camp/<int:id>', LabourCampUpdateGetDeleteView.as_view() , name =  "delete-labour-camp"),
 ]
