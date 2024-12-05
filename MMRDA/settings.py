2b7269f7f2dd046adaa02938c98e427c089c80b1
pranavkolambkar23 <pranavkolambkar23@gmail.com>
2024-12-05 12:27:00 +0530

    changed refresh , access lifespan

--- MMRDA/settings.py
+++ MMRDA/settings.py
@@ -207,8 +207,8 @@ SWAGGER_SETTINGS = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),   # 2 Hours
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),   # 2 Hours
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
