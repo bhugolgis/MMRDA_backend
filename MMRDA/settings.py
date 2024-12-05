0305c34572d67454f36f59e0caa99a187a7b2a4b
pranavkolambkar23 <pranavkolambkar23@gmail.com>
2024-12-05 10:42:16 +0530

    changed acces token time

--- MMRDA/settings.py
+++ MMRDA/settings.py
@@ -207,7 +207,7 @@ SWAGGER_SETTINGS = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),   # 2 Hours
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),   # 2 Hours
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
